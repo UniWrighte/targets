09f4957087cefc03c78aa812bd8fc376b4daa129
David Kamer <david.kamer@uniwrighte.com>
2018-12-23 23:04:50 -0500

    basic working idea

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import target from './images/target.png';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      targets: [],
+      indexes: new Set() //TODO change to set
+    }
+  }
+
+  componentDidMount(){
+
+    const targets = this.fill(12, target);
+    this.setState({targets});
+
+  }
+  fill(num, item){
+    return new Array(num).fill({}).map((x,i)=>(item));
+  }
+  down(i){
+    // const indexes = this.state.indexes.slice();
+    const indexes = new Set(this.state.indexes);
+    // const index = indexes.indexOf(i);
+    const index = indexes.has(i);
+    indexes.add(i);
+    if(index){
+      indexes.delete(i);
+    }
+    this.setState({indexes});
+    return;
+  }
   render() {
+    const {targets, indexes} = this.state;
+    console.log({indexes});
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* <img src={target} /> */}
+        {targets.map((item,i)=>{
+          return <div className="target" key={i}>
+          <img className={indexes.has(i) === false ? "targetImg up" : "targetImg down"} onClick={()=>this.down(i)} src={item}  alt="target"/>
+          </div>;
+        })}
       </div>
     );
   }
